6ef37d5f9e5e974d5657ad815e69037bdcf2157a
Smuger <krzysiek.kwietniewski@gmail.com>
2018-01-07 14:30:31 +0000

    Start,signIN,Connect frame front end

--- SharedPower.py
+++ SharedPower.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as tkk
 LARGE_FONT = ("Helvetica", 12)
+SMALL_FONT = ("Helvetica", 8)
 class SharedPower(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("Shared Power")
+        self.iconbitmap("res/favicon.ico")
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -26,25 +28,40 @@ class SharedPower(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
-        label = tk.Label(self, text="Welcome to SharedPower", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-        button1 = tk.Button(self, text="Sign in", command=lambda: controller.show_frame(SignInPage))
-        button1.pack()
-        button2 = tk.Button(self, text="Login"  , command=lambda: controller.show_frame(ConnectPage))
-        button2.pack()
-class SignInPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
         photoimage = tk.PhotoImage(file="res/tools_128.png")
         label1 = tk.Label(self, image=photoimage)
         label1.image = photoimage
         label1.pack()
-        label2 = tk.Label(self, text="Sign in", font=LARGE_FONT)
-        label2.pack(pady=10,padx=10)
+        label = tk.Label(self, text="Welcome to SharedPower", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Sign in", command=lambda: controller.show_frame(SignInPage))
+        button1.pack()
+        login = tk.Entry(self)
+        login.pack()
+        password = tk.Entry(self, show="*")
+        password.pack()
+        button2 = tk.Button(self, text="Login", command=lambda: controller.show_frame(ConnectPage))
+        button2.pack()
+class SignInPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Sign in", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
         button1 = tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage))
         button1.pack()
-        button2 = tk.Button(self, text="Login", command=lambda: controller.show_frame(StartPage))
+        label1 = tk.Label(self, text="Login:", font=SMALL_FONT)
+        label1.pack(pady=10, padx=10)
+        login = tk.Entry(self)
+        login.pack()
+        label2 = tk.Label(self, text="Password:", font=SMALL_FONT)
+        label2.pack(pady=10, padx=10)
+        password = tk.Entry(self, show="*")
+        password.pack()
+        label3 = tk.Label(self, text="Password again:", font=SMALL_FONT)
+        label3.pack(pady=10, padx=10)
+        password1 = tk.Entry(self, show="*")
+        password1.pack()
+        button2 = tk.Button(self, text="Sign in", command=lambda: controller.show_frame(StartPage))
         button2.pack()
 class ConnectPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -56,17 +73,16 @@ class ConnectPage(tk.Frame):
         self.progressbar.config(maximum=10)
         self.progressbar.pack()
 
-        button = tk.Button(self, text="Start", command=self.on_start_button_clicked)
+        def on_start_button_clicked(self):
+            self.progressbar.start(1000)
+
+        def on_stop_button_clicked(self):
+            self.progressbar.stop()
+        on_start_button_clicked(self)
+        button = tk.Button(self, text="Next", command=lambda: controller.show_frame(SearchPage))
         button.pack()
 
-        button = tk.Button(self, text="Stop", command=self.on_stop_button_clicked)
-        button.pack()
 
-    def on_start_button_clicked(self):
-        self.progressbar.start(1000)
-
-    def on_stop_button_clicked(self):
-        self.progressbar.stop()
 
 
 class SearchPage(tk.Frame):
