297d59cfc6982ba70f7ad6bc28362d0505363d08
Smuger <krzysiek.kwietniewski@gmail.com>
2018-01-08 06:18:49 +0000

    scroll not working yet

--- SharedPower.py
+++ SharedPower.py
@@ -37,7 +37,7 @@ class StartPage(tk.Frame):
         label1.pack()
         label = tk.Label(self, text="Welcome to SharedPower", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-        button1 = tk.Button(self, text="Sign in", command=lambda: controller.show_frame(SignInPage))
+        button1 = tk.Button(self, text="Sign up", command=lambda: controller.show_frame(SignInPage))
         button1.pack()
         login = tk.Entry(self)
         login.pack()
@@ -48,7 +48,7 @@ class StartPage(tk.Frame):
 class SignInPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Sign in", font=LARGE_FONT)
+        label = tk.Label(self, text="Sign up", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage))
         button1.pack()
@@ -134,13 +134,13 @@ class ResultPage(tk.Frame):
         button2 = tk.Button(self, text="Search", command=lambda: controller.show_frame(ResultPage))
         button2.pack()
         scrollbar = tkk.Scrollbar(self)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        scrollbar.pack()
 
         mylist = tk.Listbox(self, yscrollcommand=scrollbar.set)
         for line in range(100):
             mylist.insert(tk.END, "This is line number " + str(line))
 
-        mylist.pack(side=tk.LEFT, fill=tk.BOTH)
+        mylist.pack(fill=tk.BOTH)
         scrollbar.config(command=mylist.yview)
 
         #self.yScroll = tk.Scrollbar(self, orient=tk.VERTICAL)
@@ -168,6 +168,7 @@ class AddPage(tk.Frame):
         condition.pack()
         #login = tk.Entry(self)
         #login.pack()
+        label35 = tk.Label(self, text="Photo:", font=LARGE_FONT).pack()
         test = tk.PhotoImage(file="res/image_128.png")
         product_image = tk.Label(self, image=test)
         product_image.image = test
@@ -175,17 +176,70 @@ class AddPage(tk.Frame):
         label4 = tk.Label(self, text="Describe product:", font=LARGE_FONT).pack()
         description = tk.Entry(self, textvariable=DESCRIPTION_TEXT)
         description.pack()
-
+        label5 = tk.Label(self, text="Choose last day of rent:", font=LARGE_FONT).pack()
+        label6 = tk.Label(self, text="Day:", font=SMALL_FONT).pack()
+        day = tkk.Combobox(self).pack()
+        label7 = tk.Label(self, text="Month:", font=SMALL_FONT).pack()
+        month = tkk.Combobox(self).pack()
+        label8 = tk.Label(self, text="Year:", font=SMALL_FONT).pack()
+        year = tkk.Combobox(self).pack()
+        label9 = tk.Label(self, text="Price in £", font=LARGE_FONT).pack()
+        label10 = tk.Label(self, text="Per day:", font=SMALL_FONT).pack()
+        price_hour = tk.Entry(self, textvariable=DESCRIPTION_TEXT).pack()
+        label11 = tk.Label(self, text="Per month:", font=SMALL_FONT).pack()
+        price_day = tk.Entry(self, textvariable=DESCRIPTION_TEXT).pack()
+        button3 = tk.Button(self, text="Add this tool", command=lambda: controller.show_frame(SearchPage))
+        button3.pack(pady=10, padx=10)
 
 class MyToolsPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Welcome to SharedPower", font=LARGE_FONT)
+        label = tk.Label(self, text="My Tools", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
         button1 = tk.Button(self, text="Sign in", command=lambda: controller.show_frame(StartPage))
         button1.pack()
         button2 = tk.Button(self, text="Login", command=lambda: controller.show_frame(ConnectPage))
         button2.pack()
+
+        #ScrollTest
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
+        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
+        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = tk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=tk.NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', _configure_canvas)
+        #ScrollTest
+
+
 class ProductPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -215,5 +269,20 @@ class ErrorPage(tk.Frame):
         button2 = tk.Button(self, text="Login", command=lambda: controller.show_frame(ConnectPage))
         button2.pack()
 if __name__ == "__main__":
+    #ScrollTest
+    class SampleApp(tk.Tk):
+        def __init__(self, *args, **kwargs):
+            root = tk.Tk.__init__(self, *args, **kwargs)
+
+
+            self.frame = tk.SharedPower(root)
+            self.frame.pack()
+            self.label = tk.Label(text="Shrink the window to activate the scrollbar.")
+            self.label.pack()
+            buttons = []
+            for i in range(10):
+                buttons.append(tk.Button(self.frame.interior, text="Button " + str(i)))
+                buttons[-1].pack()
+    #ScrollTest
     app = SharedPower()
     app.mainloop()
