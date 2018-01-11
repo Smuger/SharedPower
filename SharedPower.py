4c3b152856724d420ef4cbe8e40888275d849616
Smuger <krzysiek.kwietniewski@gmail.com>
2018-01-11 14:40:57 +0000

    Image ex

--- SharedPower.py
+++ SharedPower.py
@@ -259,18 +259,43 @@ class ResultPage(Frame):
         search_entry = Entry(self, show="*", textvariable=SEARCH_RESULT).pack(pady=10, padx=10)
         search = Button(self, text="Search", command=lambda: controller.show_frame(ResultPage)).pack()
 
-        """
+
         # scrollbar NOT-WORK
-        scrollbar = Scrollbar(self)
-        scrollbar.pack(side=RIGHT, fill=Y)
-        record = Listbox(self, yscrollcommand=scrollbar.set)
-        for line in range(100):
-            record.insert(END, "Tool number" + str(line))
-        record.pack(fill=BOTH)
-        scrollbar.config(command=record.yview())
-        # scrollbar
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)
         # to product NOT-WORK
-        """
+
         next = Button(self, text="test// Next", command=lambda: controller.show_frame(ProductPage)).pack()
 
 class AddPage(Frame):
